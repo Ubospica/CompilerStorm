87f839d3ac6452d02bf15f74de11486ca968cbc1
Ubospica <ubospica@gmail.com>
2021-11-03 23:21:10 +0800

    SemanticChecker first version

--- semantic.bash
+++ semantic.bash
@@ -1,2 +1,4 @@
 set -e
-cat | java -cp /ulib/java/antlr-4.9.1-complete.jar:./bin Main # -semantic
+cat | java -cp /ulib/java/antlr-4.9.1-complete.jar:./bin CompilerMain.Main # -semantic
+
+#cat | java -cp ./lib/antlr-4.9.2-complete.jar:./bin CompilerMain.Main # -semantic
