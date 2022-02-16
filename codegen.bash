1f15720d9ab651a42b108d249c1b927de58de266
ubospica <ubospica@gmail.com>
2022-02-17 01:00:16 +0800

    regalloc finished

--- codegen.bash
+++ codegen.bash
@@ -2,7 +2,8 @@
 set -e
 #cd ..
 
-java -cp ./lib/antlr-4.9.2-complete.jar:./bin CompilerMain.Main -codegen
+#java -cp ./lib/antlr-4.9.2-complete.jar:./bin CompilerMain.Main -codegen
+java -cp /ulib/java/antlr-4.9.1-complete.jar:./bin CompilerMain.Main -codegen
 #clang a.ll ./src/Builtin/BuiltinFuncC.ll -Wall -Wextra
 #./a.out < tmp/a.in > tmp/a.output
 #diff tmp/a.output tmp/a.ans -Z
